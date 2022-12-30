716d7ae136d6bcbefedb09e98c2ed1316a869c87
HassanAfnan <hassanafnan20@gmail.com>
2022-12-29 23:09:53 -0800

    privacy and terms

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Privacy from "../privacy";
+import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Route, Routes} from 'react-router-dom';
+import Welcome from '../welcomepage/Welcome';
+const Navigation = () => {
+  return (
+    <Routes>
+    
+    </Routes>
+  )
+}
+
+export default Navigation
