af5a93b0c41ea7ff5334701f2aca01aa1a8bf652
HassanAfnan <hassanafnan20@gmail.com>
2023-06-26 18:45:19 +0500

    Final push

--- src/privacy.js
+++ src/privacy.js
@@ -5,7 +5,7 @@ const privacy = () => {
   return (
     <div className="App">
     <p style={{ fontSize: "30px", fontWeight: "600", textAlign: "center" }}>
-      Cannalife Culture Privacy Policy
+        Workout Santos Privacy Policy
     </p>
     <div
       style={{
@@ -16,28 +16,52 @@ const privacy = () => {
       }}
     >
       <p>
-            Any media content you share, post or import on the platform gives
-            Cannalife Culture Co and Stoniez NFTs the right to own it and use
-            it. The only information we use is the statistical data we collect
-            from the app itself, meaning content views, clicks, reactions,
-            product trends, brand popularity.{"\n\n"}2- No personal info will
-            ever be sold, period. We believe in your right to privacy, just like
-            our own. Location services are only used when a user checks in at a
-            location. {"\n\n"}3- We do not and will not Track your phone if you
-            do not have the app on.{"\n\n"}4- If there is a profile which has
-            suspicious activity or has been proven to break the law on our app,
-            we will share that profile and its information with authorities to
-            protect our community.{"\n\n"}5- You can not use Cannalife Culture
-            Co’s and StoneizNFTs brand names, logos or proprietary intellectual
-            property without ourwritten consent or under permission of open
-            source licensing.{"\n\n"}6- You can not extract code from us, our
-            sites and apps. Everything we have is proprietary, and protected by
-            Patent and Trademark.{"\n\n"}7- This includes our brand and
-            intellectual property.{"\n\n"}8- By agreeing to this policy, you
-            release Cannalife Culture Co and its owners/employees or
-            Partners/affiliates of any liability and responsibility of any legal
-            action brought against you by another person, or entity govt or
-            civilian alike.
+      Effective Date: 26 Mon 2023
+
+        This Privacy Policy describes how personal information is collected, used, and disclosed when you use our Gym Application (referred to as the "App") provided by [Your Company Name] (referred to as "we" or "us"). By using the App, you agree to the collection and use of your personal information as outlined in this Privacy Policy.
+
+        Information We Collect:
+        1.1 Personal Information:
+        We may collect certain personal information from you when you use the App, such as your name, email and other information you provide to us voluntarily.
+
+        1.2 Fitness Data:
+        With your consent, the App may collect and store fitness-related data, including your exercise routines, workout history, heart rate, distance, and other related information, to provide personalized fitness guidance and track your progress.
+
+        1.3 Device Information:
+        When you use the App, we may automatically collect certain information about your device, including your IP address, operating system, device type, unique device identifiers, and mobile network information. We may also collect information about your usage patterns within the App.
+
+        Use of Information:
+        2.1 Provide and Improve Services:
+        We use the collected information to provide, personalize, and improve the functionality of the App. This includes offering personalized workout recommendations, tracking your fitness progress, and delivering relevant content based on your preferences.
+
+        2.2 Communication:
+        We may use your personal information to communicate with you about the App, such as responding to your inquiries, sending important updates, and providing customer support.
+
+        2.3 Analytics and Aggregated Data:
+        We may use aggregated and anonymized data for statistical analysis, market research, and to improve our services. This data does not personally identify you.
+
+        Information Sharing:
+        3.1 Service Providers:
+        We may share your personal information with trusted third-party service providers who help us deliver and improve the App. These providers are bound by confidentiality agreements and are authorized to use your information only to perform specific tasks on our behalf.
+
+        3.2 Legal Compliance:
+        We may disclose your personal information if required by law or to protect our rights, safety, or property, or the rights, safety, or property of others.
+
+        Data Security:
+        We take reasonable measures to protect the security of your personal information. However, no method of transmission over the internet or electronic storage is completely secure, and we cannot guarantee absolute security.
+
+        Your Choices:
+        5.1 Opt-Out:
+        You may opt-out of receiving promotional communications from us by following the instructions in those communications. Please note that even if you opt-out, we may still send you non-promotional messages, such as those related to your account or the App.
+
+        5.2 Access and Update:
+        You may access and update your personal information by logging into your account within the App or contacting us directly.
+
+        Children's Privacy:
+        The App is not intended for use by individuals under the age of 16. We do not knowingly collect personal information from children under 16. If you become aware that a child has provided us with personal information without parental consent, please contact us, and we will take steps to remove the information and terminate the child's account.
+
+        Changes to the Privacy Policy:
+        We may update this Privacy Policy from time to time to reflect changes in our practices or for legal or regulatory reasons. We will notify you of any material changes by posting the updated Privacy Policy on our website or within the App. Your continued use of the App after such modifications will signify your acceptance of the updated Privacy Policy.
       </p>
     </div>
   </div>
